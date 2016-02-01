18cd3720586f788bd74df095b02de86e7754384d
BillChabzz <chaby9.8@gmail.com>
2016-02-01 23:41:05 +0300

    success test animations using greensock

---
+++ js/test.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+var toggle = $(".open"),
+    about = $("#about");
+    
+    toggle.click(function(){
+       TweenMax.from(about, 1 ,{
+           x : 1000,
+           autoAlpha: 0,
+           
+       }) 
+    });
+    
+});
+
